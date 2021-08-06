423113729c005e11ac4bcb3e2dfe9a0c4ec1d232
JD Patel <iamjdpatel@gmail.com>
2021-08-06 16:13:12 +0530

    initial code

--- Sources/Loggr/Loggr.swift
+++ Sources/Loggr/Loggr.swift
@@ -1,3 +1,30 @@
-struct Loggr {
-    var text = "Hello, World!"
+import Foundation
+import os.log
+
+public final class Loggr {
+            
+    public static func logDebug(_ msg: String) {
+        #if DEBUG
+        os_log(.debug, "[DEBUG]: %s", msg)
+        #endif
+    }
+    
+    public static func logInfo(_ msg: String) {
+        #if DEBUG
+        os_log(.debug, "[INFO]: %s", msg)
+        #endif
+    }
+    
+    public static func logWarning(_ msg: String) {
+        #if DEBUG
+        os_log(.fault, "[WARNING]: %s", msg)
+        #endif
+    }
+    
+    public static func logError(_ msg: String) {
+        #if DEBUG
+        os_log(.error, "[ERROR]: %s", msg)
+        #endif
+    }
+
 }
--- Tests/LoggrTests/LoggrTests.swift
+++ Tests/LoggrTests/LoggrTests.swift
@@ -1,11 +1,47 @@
-    import XCTest
-    @testable import Loggr
+import XCTest
+@testable import Loggr
 
-    final class LoggrTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(Loggr().text, "Hello, World!")
-        }
+final class LoggrTests: XCTestCase {
+    func testLogDebugSuccess() {
+        let exp: XCTestExpectation = expectation(description: "Log Debug get called with no error")
+        
+        Loggr.logDebug("Debug Message")
+        exp.fulfill()
+
+        wait(for: [exp], timeout: 1.0)
     }
+    
+    func testLogInfoSuccess() {
+        let exp: XCTestExpectation = expectation(description: "Log Info get called with no error")
+        
+        Loggr.logInfo("Info Message")
+        exp.fulfill()
+
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func testLogWarningSuccess() {
+        let exp: XCTestExpectation = expectation(description: "Log Warning get called with no error")
+        
+        Loggr.logWarning("Warning Message")
+        exp.fulfill()
+
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func testLogErrorSuccess() {
+        let exp: XCTestExpectation = expectation(description: "Log Error get called with no error")
+        
+        Loggr.logError("Error Message")
+        exp.fulfill()
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    static var allTests = [
+        ("testLogDebugSuccess", testLogDebugSuccess),
+        ("testLogInfoSuccess", testLogInfoSuccess),
+        ("testLogWarningSuccess", testLogWarningSuccess),
+        ("testLogErrorSuccess", testLogErrorSuccess),
+    ]
+}
